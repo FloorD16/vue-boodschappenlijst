b6091c892395acdbeebd5d121d0b46c3fc8b2515
FloorD16 <floor.dogger@gmail.com>
2025-07-08 12:46:58 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue';
+
+import GroceriesTable from './components/GroceriesTable.vue';
+
+const products = ref([{ item: 'Rijst', price: 1, amount: 0 },
+    { item: 'Broccoli', price: 0.99, amount: 0 },
+    { item: 'Koekjes', price: 1.20, amount: 0 },
+    { item: 'Noten', price: 2.99, amount: 0 }
+ ]);
+</script>
+
+<template>  
+  <GroceriesTable :products="products" />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/GroceriesTable.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { computed } from 'vue';
+
+const { products } = defineProps({products: Array});
+
+const totalCosts = computed(() => {
+  return products.value.reduce((sum, product) => sum + product.price * product.amount, 0).toFixed(2);
+  }
+);
+</script>
+
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>Product</th>
+        <th>Prijs</th>
+        <th>Aantal</th>
+        <th>Subtotaal</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="product in products" :key="product.item">
+        <td class="align-left">{{ product.item }}</td>
+        <td class="align-right">{{ product.price.toFixed(2) }}</td>
+        <td class="align-left"><input v-model="product.amount" type="number" min="0" step="1" /></td>
+        <td class="align-right">{{ (product.price * product.amount).toFixed(2) }}</td>
+      </tr>
+    </tbody>
+    <tfoot>
+      <tr>
+        <td class="align-left" colspan=3><strong>Totaal</strong></td>
+        <td class="align-right">{{ totalCosts }}</td>
+      </tr>
+    </tfoot>
+  </table>
+</template>
+
+<style scoped>
+table {
+  border-collapse: collapse;
+}
+
+th, td {
+  border: 1px solid black;
+  padding: 10px;
+}
+
+.align-left {
+  text-align: left;
+}
+
+.align-right {
+  text-align: right;
+}
+</style>
